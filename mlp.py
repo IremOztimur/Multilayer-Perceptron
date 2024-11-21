e0370a6ec038c563eb167fb26d461a85d11e96e4
IremOztimur <iremoztimur07@gmail.com>
2024-11-21 22:03:44 +0300

    [ADD] train_test_split implemented

--- mlp.py
+++ mlp.py
@@ -1,15 +1,21 @@
 import pandas as pd
-from srcs.preprocess.eda import get_preprocessed_data
+from srcs.preprocess.data import get_preprocessed_data, train_test_split
 from srcs.preprocess.one_hot_encoder import to_categorical
-from sklearn.preprocessing import RobustScaler
 
 
 if __name__ == '__main__':
     df = get_preprocessed_data()
     
-    x = df.values[:, 1:]
+    X = df.values[:, 1:]
     y = df['diagnosis'].values
     y = to_categorical(y)
     
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
+    # Print the shapes to verify
+    print("X_train shape:", X_train.shape)
+    print("X_test shape:", X_test.shape)
+    print("y_train shape:", y_train.shape)
+    print("y_test shape:", y_test.shape)
+
 
