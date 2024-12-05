5c9d8b4031d10725b0958ccbf55044e54c9d7bc3
IremOztimur <iremoztimur07@gmail.com>
2024-12-05 18:40:54 +0300

    [UPDATE] expanded by including activation function operations to the class

--- srcs/model/layer.py
+++ srcs/model/layer.py
@@ -1,11 +1,16 @@
 import numpy as np
+from activation import ReLU, Softmax
 from abc import ABC, abstractmethod
 
 
 class Layer(ABC):
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons, activation):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.n_inputs = n_inputs
+        self.n_neurons = n_neurons
+        self.activation_function = activation
+        self.activation_derivative = None
     
     @abstractmethod
     def forward(self, inputs):
@@ -13,8 +18,11 @@ class Layer(ABC):
         
 
 class Dense(Layer):
-    def __init__(self, n_inputs, n_neurons):
-        super().__init__(n_inputs, n_neurons)
+    def __init__(self, n_inputs, n_neurons, activation):
+        super().__init__(n_inputs, n_neurons, activation)
         
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+        self.linear_output = np.dot(inputs, self.weights) + self.biases
+        self.activation_function.forward(self.linear_output)
+        self.output = self.activation_function.output
