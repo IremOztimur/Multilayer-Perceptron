[FIX] backpropagation corrected

srcs/main.py
@@ -24,13 +24,13 @@ def main():
 
 
     nn = NeuralNetwork()
-    nn.optimizer = SGD(learning_rate=0.0001, momentum=0.9, decay=1e-4)
+    nn.optimizer = SGD(learning_rate=0.005, momentum=0.9, decay=1e-4)
     # nn.optimizer = SGD(learning_rate=0.0005) # vanillia SGD
     # nn.optimizer = Adam(learning_rate=0.0003, decay=1e-3)
     # nn.optimizer = RMSProp(learning_rate=0.001, rho=0.9, epsilon=1e-7, decay=0.01)
     nn.add(Dense(n_inputs=X_train.shape[1], n_neurons=16, activation=ReLU(), initializer='He'))
-    nn.add(Dense(n_inputs=16, n_neurons=8, activation=Sigmoid(), initializer='Xavier'))
-    nn.add(Dense(n_inputs=8, n_neurons=2, activation=Softmax(), initializer='Xavier'))
+    nn.add(Dense(n_inputs=16, n_neurons=16, activation=Sigmoid(), initializer='Xavier'))
+    nn.add(Dense(n_inputs=16, n_neurons=2, activation=Softmax(), initializer='Xavier'))
     
     print("Training...") 
     history = nn.train(X_train, y_train, n_epochs=50, batch_size=16, validation_data=(X_test, y_test), patience=3)
@@ -38,7 +38,7 @@ def main():
     print("Testing...") 
     
     y_pred = nn.predict(X_test)
-    # print(f"Predicted labels: {y_pred}")
+    print(f"Predicted labels: {y_pred}")
     
     test_accuracy = np.mean(y_pred == np.argmax(y_test, axis=1))
     y_true = np.argmax(y_test, axis=1)

srcs/model/activation.py
@@ -28,11 +28,10 @@ class Softmax(Activation):
         return self
     
     def backward(self, gradients, y_true):
-        samples = len(gradients)
-
-        self.dinputs = gradients - y_true
+        samples = len(y_true)
+        self.dinputs = self.output
+        self.dinputs[range(samples), np.argmax(y_true, axis=1)] -= 1
         self.dinputs = self.dinputs / samples
-        
 
 class Sigmoid(Activation):
     def forward(self, inputs):

srcs/model/layer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import ABC, abstractmethod
+from model.activation import Softmax
 
 
 class Layer(ABC):
@@ -33,7 +34,11 @@ class Dense(Layer):
         self.activation_function.forward(self.linear_output)
         self.output = self.activation_function.output
 
-    def backward(self, gradients):
-        self.dweights = np.dot(self.inputs.T, gradients)
-        self.dbiases = np.sum(gradients, axis=0, keepdims=True)
-        self.dinputs = np.dot(gradients, self.weights.T)
+    def backward(self, gradients, y_true=None):
+        if isinstance(self.activation_function, Softmax) and y_true is not None:
+            self.activation_function.backward(gradients, y_true)
+        else:
+            self.activation_function.backward(gradients)
+        self.dweights = np.dot(self.inputs.T, self.activation_function.dinputs)
+        self.dbiases = np.sum(self.activation_function.dinputs, axis=0, keepdims=True)
+        self.dinputs = np.dot(self.activation_function.dinputs, self.weights.T)